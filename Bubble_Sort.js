49bd7b7b694db01f56e3ca837bf202aa58311ed2
sayali giri <sayligiri555@gmail.com>
2024-03-20 23:03:36 +0530

    bubble sort

---
+++ Bubble_Sort.js
@@ -0,0 +1,25 @@
+// Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
+// Example 1:
+
+// Input: 
+// N = 5
+// arr[] = {4, 1, 3, 9, 7}
+// Output: 
+// 1 3 4 7 9
+
+class Solution
+{
+   //Function to sort the array using bubble sort algorithm.
+   bubbleSort(arr,n){
+    //code here
+    for(let i=0;i<n;i++){
+        for(let j=0;j<n;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+            }
+        }
+    }
+    return arr
+   }
+    
+}
