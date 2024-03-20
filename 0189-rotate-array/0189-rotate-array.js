b80cdf66379e9c3007ce137869bb6dca20aec076
Sayli Giri <96167212+Sayli555@users.noreply.github.com>
2024-03-20 19:27:55 +0530

    Time: 80 ms (62.15%), Space: 62.7 MB (5.20%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+
+k=k%nums.length
+swap(nums,0,nums.length-1)
+swap(nums,0,k-1)
+swap(nums,k,nums.length-1)
+return nums
+};
+
+function swap(arr,start,end){
+
+while(start<end){
+[arr[start],arr[end]]=[arr[end],arr[start]]
+start++
+end--
+}
+}
