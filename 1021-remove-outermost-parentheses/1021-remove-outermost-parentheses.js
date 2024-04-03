e762474bfa88d2426c1f0c1f63cf634b8bcbe9d5
Sayli Giri <96167212+Sayli555@users.noreply.github.com>
2024-04-03 16:05:23 +0530

    Time: 53 ms (78.88%), Space: 50.1 MB (93.75%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let count=0
+let res=""
+for(let i=0;i<s.length;i++){
+  if(s[i]=="("){
+    if(count>0){
+      res+=s[i]
+    }
+    count++
+  }
+  else if(s[i]==")"){
+    if(count>1){
+      res+=s[i]
+    }
+    count--
+  }
+}
+return res
+};
